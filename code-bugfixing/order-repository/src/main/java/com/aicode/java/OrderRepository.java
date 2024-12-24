7a06785e4d4abab0e2603db849ca45536b226260
Palina Talkachova <polina.winner@gmail.com>
2024-12-24 14:02:27 +0300

    Update OrderRepository

--- code-bugfixing/order-repository/src/main/java/com/aicode/java/OrderRepository.java
+++ code-bugfixing/order-repository/src/main/java/com/aicode/java/OrderRepository.java
@@ -9,10 +9,10 @@ import java.time.LocalDateTime;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("SELECT o FROM Order o " +
+    @Query("SELECT o FROM Orders o " +
         "WHERE (:status IS NULL OR o.status = :status) " +
         "AND (:customerId IS NULL OR o.customerId = :customerId) " +
-        "AND (CAST(:minTotalCost AS double) IS NULL OR o.totalCost >= :minTotalCost) " +
+        "AND (CAST(:minTotalCost AS double) IS NULL OR o.totalCost >= :minTotalCost " +
         "AND (CAST(:maxTotalCost AS double) IS NULL OR o.totalCost <= :maxTotalCost) " +
         "AND (:startDate IS NULL OR o.orderDate >= :startDate) " +
         "AND (:endDate IS NULL OR o.orderDate <= :endDate)")
