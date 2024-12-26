65558b35411d1f1cb177a00093f9d194dc2c880e
Palina Talkachova <polina.winner@gmail.com>
2024-12-26 17:53:41 +0300

    Update PasswordValidator.ts

--- code-bugfixing/password-validator/TypeScript/VSCode/src/PasswordValidator.ts
+++ code-bugfixing/password-validator/TypeScript/VSCode/src/PasswordValidator.ts
@@ -2,12 +2,12 @@ export class PasswordValidator {
 
     // Regular expression for a strong password
     private static readonly PASSWORD_REGEX: RegExp = new RegExp(
-        "^(?=.*[0-9])" +
+        "^(?=[0-9])" +
         "(?=.*[a-z])" +
         "(?=.*[A-Z])" +
-        "(?=.*[@#$%^&+=!])" +
+        "(?=.*[@#$%^&+=])" +
         "(?=\\S+$)" +
-        ".{8,}$"
+        ".{8}$"
     );
 
     public static isValidPassword(password: string | null): boolean {
