a0b701bee83e6004815e8050fe013245e1210c46
Palina Talkachova <polina.winner@gmail.com>
2024-12-27 16:27:33 +0300

    Add order processor test

---
+++ code-bugfixing/order-processor/java/src/main/java/com/aicode/java/Product.java
@@ -0,0 +1,28 @@
+package com.aicode.java;
+
+class Product {
+
+    String productId;
+    String productName;
+    int quantity;
+    double price;
+
+    public Product(String productId, String productName, int quantity, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+}
